1bd564980c0574f39ac9756f41977685f0187e4d
emp1tsu <oneok0813@gmail.com>
2021-02-22 01:00:30 +0900

    Adding CORS to our Serverless API

--- libs/handler-lib.js
+++ libs/handler-lib.js
@@ -3,7 +3,6 @@ export default function handler(lambda) {
     let body, statusCode;
 
     try {
-      // Run the Lambda
       body = await lambda(event, context);
       statusCode = 200;
     } catch (e) {
@@ -11,10 +10,13 @@ export default function handler(lambda) {
       statusCode = 500;
     }
 
-    // Return HTTP response
     return {
       statusCode,
       body: JSON.stringify(body),
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
     };
   };
 }
